d3348266a06c7a07ad8e2661db270c463d15f021
rizalk12 <85008933+rizalk12@users.noreply.github.com>
2023-01-31 10:07:54 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -11,7 +11,7 @@ export const $themeConfig = {
   app: {
     appName: '0xarikun', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/logox.svg', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/logo0xarikun.svg', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
